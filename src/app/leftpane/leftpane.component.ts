9cb6f0edb18cbfe24cc39b22efcdc3cd21417e8f
Mukesh Maurya <mukeshmaurya@Mukeshs-MacBook-Pro.local>
2021-06-26 14:32:38 +0530

    Replace input/output decorator with a service, add styles to toggle button

--- src/app/leftpane/leftpane.component.ts
+++ src/app/leftpane/leftpane.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { SidebarsService } from '../services/sidebars.service';
 
 @Component({
   selector: 'app-leftpane',
@@ -9,13 +10,17 @@ export class LeftpaneComponent implements OnInit {
   // variable to store value of the tab button clicked
   tab: string = 'dashboard';
 
+  closePane: boolean = true;
+
   // Creates an instance of eventemitter and broadcasts to parent component
   @Output() passTabValue = new EventEmitter<any>();
+  displayNav: boolean = false;
+  // @Output() passCloseState = new EventEmitter<any>();
 
-  @Input() displayNav: any;
+  // @Input() displayNav: any;
 
-  // data recived after toggle button click
-  @Input() showPane: any;
+  // // data recived after toggle button click
+  // @Input() showPane: any;
 
   // Sends the value of tab variable whenever it changes
   sendTabValue(value: string): void {
@@ -23,7 +28,13 @@ export class LeftpaneComponent implements OnInit {
     this.passTabValue.emit(this.tab);
   }
 
-  constructor() {}
+  functionClosePane(): void {
+    this.sidebarsService.displayLeftPane = false;
+  }
 
-  ngOnInit(): void {}
+  constructor(private sidebarsService: SidebarsService) {}
+
+  ngOnInit(): void {
+    this.displayNav = this.sidebarsService.displayLeftPane;
+  }
 }
